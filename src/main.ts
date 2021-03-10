b8ad91d7f711b7a0f4c0b99244f042949ad3bb54
root <root@master.192.168.0.142.nip.io>
2021-03-10 15:13:24 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-
+import { init as initApm } from "@elastic/apm-rum";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
+const apm = initApm({
 
+  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
+  serviceName: '',
+
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'http://172.30.157.125:8200',
+
+  // Set service version (required for sourcemap feature)
+  serviceVersion: ''
+})
 if (environment.production) {
   enableProdMode();
   // override the console.log in the client side
