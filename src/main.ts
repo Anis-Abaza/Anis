1b6c56302c9395d9f486f245dd3b2d352c3e4fbb
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-03-11 10:14:34 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { environment } from "./environments/environment";
 const apm = initApm({
 
   // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: '',
+  serviceName: 'test',
 
   // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://172.30.157.125:8200',
+  serverUrl: 'http://172.30.121.140:8200',
 
   // Set service version (required for sourcemap feature)
   serviceVersion: ''
